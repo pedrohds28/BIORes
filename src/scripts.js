1aa026886bd1cb84b79a3152649103eb94f19809
PedroHDS <pedroh.dsilva28@gmail.com>
2026-01-09 17:30:07 -0300

    Adicionando conteúdo da página inicial, conectando com a API de salvamento e adicionando lógica de salvamento de favoritos

--- src/scripts.js
+++ src/scripts.js
@@ -16,3 +16,27 @@ const _closeMenu = () => {
 
 closeMenuBtn.addEventListener('click', _closeMenu);
 overlay.addEventListener('click', _closeMenu);
+
+// Save as Favorite Logic
+async function favoritar(user, title) {
+    const url = window.location.pathname;
+
+    const data = {
+        user: user,
+        title: title,
+        url: url
+    };
+
+    try {
+        const response = await fetch('http://localhost:8080/favorites', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        alert(result.message);
+    } catch (error) {
+        console.error("Erro na conexão com a API!", error);
+    }
+}
